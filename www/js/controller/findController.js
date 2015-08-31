6920fea0e0dcb5e5fb3b8edfcca8ab1b60ff9d35
Son Nguyen <hoangson.2411@gmail.com>
2015-08-31 16:24:35 +0700

    add style for find

--- www/js/controller/findController.js
+++ www/js/controller/findController.js
@@ -1,21 +1,24 @@
 EzRoommateApp.controller('findController',['$scope','$state','$timeout', '$ionicBackdrop', '$ionicModal', '$ionicSlideBoxDelegate', '$ionicScrollDelegate',function($scope,$state,$timeout, $ionicBackdrop, $ionicModal, $ionicSlideBoxDelegate, $ionicScrollDelegate){
 
 
+
 	$scope.imgs =["img/demo-room.jpg", "img/demo-room-2.jpg"]
 	$scope.tags = ['ok1', 'ok2'];
-
+	$scope.state=0;
+	console.log($scope.state);
 	$scope.users=[{
 		name:"Artour \"Arteezy\" Babaev",
 		age:"19",
-		img:"img/user3_avatar.jpg"
+		img:"img/user3_avatar.jpg",
+		description:"Hi, I'm just a boy play Dota 2"
 	},
 	{
 		name:"Clinton \"Fear\" Loomis",
 		age:"27",
-		img:"img/user2_avatar.jpg"
+		img:"img/user2_avatar.jpg",
+		description:"Hi, I'm just a man play Dota 2"
 	}
 	];
-
 	$scope.zoomMin = 1;
 	$scope.activeSlide =0
 
@@ -23,5 +26,10 @@ EzRoommateApp.controller('findController',['$scope','$state','$timeout', '$ionic
 		console.log(index);
 		$scope.viewProfile();
 	}
-
+	$scope.changeStyle = function(){
+		if($scope.state==0)
+			$scope.state=1;
+		else
+			$scope.state=0;
+	}
 }]);
